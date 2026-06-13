5080c831f945ff251415537fbb95c90e7ab751b9
Araj Ansari <mohderajansari@gmail.com>
2026-06-13 22:20:49 +0530

    Learning React

---
+++ LearningReact/calculator - Copy.js
@@ -0,0 +1,9 @@
+
+export let sum = (a, b) => {
+    return a + b;
+}
+export let multi = (x, y) => {
+    return x * y;
+}
+export let output = "Hello Welcome.. To My Webpages.....";
+
---
+++ LearningReact/main - Copy.js
@@ -0,0 +1,5 @@
+import { multi, output, sum } from "./calculator.js";
+
+console.log(sum(30, 20));
+console.log(multi(5, 10));
+console.log(output);
